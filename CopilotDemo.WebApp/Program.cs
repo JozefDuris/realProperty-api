235f6c13643f08657544c829eecfb0cb83bcbd06
Claudiu Nicola <claudiu.nicola@gmail.com>
2025-09-08 18:05:05 +0300

    Webapp (Blazor)

---
+++ CopilotDemo.WebApp/Program.cs
@@ -0,0 +1,33 @@
+using CopilotDemo.WebApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Add HttpClient for API calls
+builder.Services.AddHttpClient("WebApi", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7200/"); // Update with actual API URL
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
